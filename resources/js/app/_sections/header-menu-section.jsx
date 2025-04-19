c4643f7a29f913688549741e45fb5f10a2edef3b
QuicklyDG <quicklydeguzman@gmail.com>
2025-04-19 16:38:25 +0800

    change user icon

--- resources/js/app/_sections/header-menu-section.jsx
+++ resources/js/app/_sections/header-menu-section.jsx
@@ -16,7 +16,7 @@ export default function HeaderMenuSection({ userNavigation }) {
                 <span className="sr-only">Open user menu</span>
                 <img
                     alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src="/images/user.png"
                     className="size-8 rounded-full bg-gray-50"
                 />
                 <span className="hidden lg:flex lg:items-center">
