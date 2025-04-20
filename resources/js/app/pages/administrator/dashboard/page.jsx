e31b18e4f7b1c663dfac93ed62ed4dcc2cef5d35
QuicklyDG <quicklydeguzman@gmail.com>
2025-04-20 14:07:37 +0800

    added dashboard cards

--- resources/js/app/pages/administrator/dashboard/page.jsx
+++ resources/js/app/pages/administrator/dashboard/page.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
 import Button from "@/app/_components/button";
 import Input from "@/app/_components/input";
-import { H1Icon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { ComputerDesktopIcon, H1Icon, UserCircleIcon } from "@heroicons/react/24/outline";
 import TextArea from "@/app/_components/textarea";
 import Radio from "@/app/_components/radio";
 import Modal from "@/Components/Modal";
 import AdminLayout from "../layout";
+import DashboardCardSections from "./sections/dashboard-card-sections";
 
 export default function Page() {
     const [open, setOpen] = useState(false);
     return (
         <AdminLayout>
+            <div className="flex gap-6 mb-9">
+                <DashboardCardSections
+                    title="Sales"
+                    icon={<ComputerDesktopIcon className="h-14 text-pink-600" />}
+                />
+                <DashboardCardSections
+                    title="Sales"
+                    icon={<ComputerDesktopIcon className="h-14 text-pink-600" />}
+                />
+                <DashboardCardSections
+                    title="Sales"
+                    icon={<ComputerDesktopIcon className="h-14 text-pink-600" />}
+                />
+                <DashboardCardSections
+                    title="Sales"
+                    icon={<ComputerDesktopIcon className="h-14 text-pink-600" />}
+                />
+            </div>
             <div className="flex gap-3">
                 <Button loading={true} variant="primary">
                     hello
---
+++ resources/js/app/pages/administrator/dashboard/sections/dashboard-card-sections.jsx
@@ -0,0 +1,31 @@
+import { Link } from '@inertiajs/react'
+import React from 'react'
+
+export default function DashboardCardSections({ icon, title, href, link }) {
+    return (
+        <div class="flex flex-1">
+            <div class="group relative overflow-hidden bg-white px-6 pt-10 pb-8 shadow-2xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:rounded-lg sm:px-10 w-full">
+                <span class="absolute top-10  h-20 w-20 rounded-full bg-pink-300 transition-all duration-300 group-hover:scale-[10]"></span>
+                <div class="relative  mx-auto w-full flex flex-col justify-between">
+                    <span class="grid h-20 w-20 place-items-center rounded-full bg-pink-300 transition-all duration-300 group-hover:bg-pink-200">
+                        {icon}
+                    </span>
+                    <div class="space-y-6 pt-5 text-base leading-7 text-gray-400 transition-all duration-300 group-hover:text-white/90">
+                        <p>{title}</p>
+                    </div>
+                    <div class="pt-5 text-base font-semibold leading-7">
+                        <p>
+                            <Link
+                                href={link}
+                                class="text-pink-300 transition-all duration-300 group-hover:text-white"
+                            >
+                                {href}
+                                &rarr;
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
