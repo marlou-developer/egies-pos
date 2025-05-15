b46114f845dcdf6cc22bb0107c36d63532635184
Marlou Dev <marlou.dev.app@gmail.com>
2025-05-15 21:11:09 +0800

    added model controller migration for product

---
+++ app/Models/ProductDiscount.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductDiscount extends Model
+{
+    //
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'discount',
+    ];
+}
