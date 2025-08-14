6e45d21ed4c919bf64d6f98c1fe3cb865f87ad34
QuicklyDG <quicklydeguzman@gmail.com>
2025-08-14 09:03:15 +0800

    adjust number pos icon

--- resources/js/app/pages/administrator/pos/sections/pos-section.jsx
+++ resources/js/app/pages/administrator/pos/sections/pos-section.jsx
@@ -195,9 +195,11 @@ export default function PosSection() {
                                 <div className="flex gap-3 ">
                                     <div className="relative inline-block">
                                         <ShoppingCartIcon className="h-8 w-12 text-pink-400" />
-                                        <div className="absolute -top-2 -right-2 bg-pink-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-xs font-bold">
-                                            {carts.length}
-                                        </div>
+                                        {carts.length > 0 && (
+                                            <div className="absolute -top-2 -right-2 bg-pink-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-xs font-bold">
+                                                {carts.length}
+                                            </div>
+                                        )}
                                     </div>
 
                                     <button
